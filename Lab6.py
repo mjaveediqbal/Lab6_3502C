e5ad8c37c3193f8260f7be601b6a79b2fb075fb9
J2304789 <88808883+J2304789@users.noreply.github.com>
2023-03-08 12:34:54 -0500

    Added Main function

--- Lab6.py
+++ Lab6.py
@@ -20,3 +20,41 @@ def encode(password):
         new_password=new_password+str(x)
 
     return new_password
+
+#Prints menu options
+def menu_print():
+    print(
+'''
+Menu
+-------------
+1. Encode
+2. Decode
+3. Quit
+
+'''
+)
+
+#Main function
+def main():
+    new_password=0
+    menu_option=1
+    #Exits function when user specifies a value greater than or equal to 3 or less than or equal to 0
+    while menu_option<=2 and menu_option>0:
+        menu_print()
+
+        menu_option=int(input("Please enter an option: "))
+
+        if menu_option==1:
+            #Stores encoded password 
+            password=input("Please enter your password to encode: ")
+            new_password=encode(password)
+            print("Your password has been encoded and stored!")
+        #Checks if there was a encoded password, and if so, prints out encoded password and decoded password
+        if menu_option==2:
+            if new_password==0:
+                print("No password has been encoded yet")
+            else:
+                print(f"The encoded password is {new_password}, and the original password is .")#add decode function
+
+if __name__=='__main__':
+    main()
